38c06ab63d6c8e47f6719e2d809f86c5103da6a1
Sunitha Iyadurai <>
2021-05-10 03:21:33 +0000

    JavaScript Exceptions 17.4.1 Completed

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+const input = require('readline-sync');
+
+let numerator = Number(input.question("Enter numerator:"));
+let denominator=Number(input.question("Enter denominator:"));
 
 // Your function should return the result of numerator / denominator.
 
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+function divide (a,b)
+{
+  let result=0;
+  if(denominator===0){
+    throw SyntaxError("Attempted to divide by zero.");
+  }
+  else{
+  result=numerator/denominator;
+  return result;
+  }
+}
+console.log(divide(numerator,denominator));
